cb673fc24cf0f2ac4499f9bcf83dbd41e7f7f38f
JsMayerMin <js.mayer.min@gmail.com>
2017-09-22 22:30:59 +0900

    refactor code

--- Udacity.Python/Lecture3/turtules.py
+++ Udacity.Python/Lecture3/turtules.py
@@ -1,21 +1,24 @@
 import turtle
 
 def draw_square() :
-    window = turtle.Screen()
-
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
 
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+
+def draw_art() :
+    window = turtle.Screen()
+
+    draw_square()
 
     angie = turtle.Turtle()
     angie.shape("arrow")
@@ -29,5 +32,5 @@ def draw_square() :
         angie2.right(120)
 
 
-draw_square()
+draw_art()
 
