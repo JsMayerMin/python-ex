b64b30cd8403bb39cf1f40ded151339dbad0a0d1
JsMayerMin <js.mayer.min@gmail.com>
2017-09-22 22:21:03 +0900

    example2

--- Udacity.Python/Lecture3/turtules.py
+++ Udacity.Python/Lecture3/turtules.py
@@ -4,7 +4,7 @@ def draw_square() :
     window = turtle.Screen()
 
     brad = turtle.Turtle()
-        brad.shape("turtle")
+    brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
 
@@ -16,6 +16,11 @@ def draw_square() :
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.circle(100)
+
 
 draw_square()
 
