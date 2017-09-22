2289332014e369256f4987dc8274a4a96da11abe
JsMayerMin <js.mayer.min@gmail.com>
2017-09-22 22:16:41 +0900

    turtle example

--- Udacity.Python/Lecture3/turtules.py
+++ Udacity.Python/Lecture3/turtules.py
@@ -1,4 +1,21 @@
-#test
+import turtle
 
+def draw_square() :
+    window = turtle.Screen()
 
+    brad = turtle.Turtle()
+        brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+draw_square()
 
