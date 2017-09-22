327c44db05eca08e55cc07ef9f8d027be8debcf3
JsMayerMin <js.mayer.min@gmail.com>
2017-09-22 22:36:04 +0900

    draw circle by using sqaure

--- Udacity.Python/Lecture3/turtules.py
+++ Udacity.Python/Lecture3/turtules.py
@@ -1,10 +1,7 @@
 import turtle
 
-def draw_square() :
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("yellow")
-    brad.speed(2)
+def draw_square(brad) :
+
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -18,7 +15,14 @@ def draw_square() :
 def draw_art() :
     window = turtle.Screen()
 
-    draw_square()
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(10)
+
+    for i in range(36):
+        draw_square(brad)
+        brad.right(10)
 
     angie = turtle.Turtle()
     angie.shape("arrow")
